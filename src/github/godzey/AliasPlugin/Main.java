a2e622daedd67c161b02244ae2ebd46516d9bcc7
Godzey <Godzey@1337.no>
2014-01-17 10:12:39 +0100

    Setting up basic startup.

---
+++ src/github/godzey/AliasPlugin/Main.java
@@ -0,0 +1,22 @@
+package github.godzey.AliasPlugin;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+	@Override
+	public void onEnable(){
+		
+	}
+	@Override
+	public void onDisalbe(){
+		
+	}
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+		
+		return false;
+	}
+}
