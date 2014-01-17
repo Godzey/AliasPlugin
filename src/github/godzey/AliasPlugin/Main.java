0e940927b4640a6e134ec45ea5462fb76bb98b02
Godzey <Godzey@1337.no>
2014-01-17 10:52:00 +0100

    Soon...

--- src/github/godzey/AliasPlugin/Main.java
+++ src/github/godzey/AliasPlugin/Main.java
@@ -1,20 +1,36 @@
 package github.godzey.AliasPlugin;
 
+import java.io.File;
+import java.util.logging.Logger;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
-
+	
+	private Logger log = Logger.getLogger("Minecraft");
+	File myfile = new File("aliases.txt");
+	
 	@Override
 	public void onEnable(){
+		log.info("AliasPlugin has started.");
 		
+		if(myfile.exists()) {
+			//do wat?
+		}
+		else {
+			log.info("Error: aliases.txt is not found!");
+		}
 	}
+	
 	public void onDisalbe(){
-		
+		log.info("AliasPlugin has stopped.");
 	}
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+		
+		
 		
 		return false;
 	}
